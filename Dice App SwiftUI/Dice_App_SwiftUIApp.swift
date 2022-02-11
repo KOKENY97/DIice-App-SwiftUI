a12d7faf92a3b8415e2329eae72416ecf9787cb5
Cahue Kokeny <cahue.kokeny@hotmail.com>
2022-02-11 08:51:55 -0500

    first commit

---
+++ Dice App SwiftUI/Dice_App_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_App_SwiftUIApp.swift
+//  Dice App SwiftUI
+//
+//  Created by user209843 on 2/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Dice_App_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# DIice-App-SwiftUI
